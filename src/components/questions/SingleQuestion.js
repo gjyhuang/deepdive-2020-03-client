4354adb60e18b1325c2b9a7b92e7b3de4cb24d98
gjyhuang <grace.jy.huang@gmail.com>
2020-04-19 18:50:40 -0400

    Day 07 - front end p2: async for each re: questionoptions

--- src/components/questions/SingleQuestion.js
+++ src/components/questions/SingleQuestion.js
@@ -6,7 +6,7 @@ import { Typeahead } from 'react-bootstrap-typeahead';
 
 const SingleQuestion = (props) => {
   const [question, setQuestion] = React.useState({});
-  const [allOptions, setAllOptions] = React.useState({});
+  const [allOptions, setAllOptions] = React.useState([]);
   const [optionsToAdd, setOptionsToAdd] = React.useState([]);
 
   const getQuestion = async () => {
@@ -24,26 +24,15 @@ const SingleQuestion = (props) => {
   // console.log('all options >>>', allOptions)
 
   const handleSubmit = async () => {
-    // const makeAssociation = () => {
-
-    // }
-
-    // async function asyncForEach(array, callback) {
-    //   for (let index = 0; index < array.length; index++) {
-    //     await callback(array[index], index, array);
-    //   }
-    // }
-
-    // console.log('options to add?', optionsToAdd);
-    // optionsToAdd.forEach(option => {
-    //   const dataToSend = {
-    //     questionId: question.id,
-    //     optionId: option.id,
-    //     isAnswer: false
-    //   }
-    //   await axios.post("/api/questionOptions", dataToSend);
-    //   getQuestion();
-    // })
+    optionsToAdd.forEach(async (option) => {
+      const dataToSend = {
+        questionId: question.id,
+        optionId: option.id,
+        isAnswer: false
+      }
+      await axios.post("/api/questionOptions", dataToSend);
+      getQuestion();
+    })
   }
 
   React.useEffect(() => {
@@ -72,8 +61,8 @@ const SingleQuestion = (props) => {
         <h3>Options:</h3>
         {!question?.options?.length ? "None associated." : null}
         {question?.options?.map((option) => (
-          <div className="question__single-option">
-            {option.body} {option.id}
+          <div key={option.id} className="question__single-option">
+            {option.body} - id: {option.id}
           </div>
         ))}
       </div>
@@ -81,15 +70,13 @@ const SingleQuestion = (props) => {
         <h3>Add options to this question:</h3>
         <div className="question__typeahead">
           <Typeahead
-            // id="basic-typeahead-example"
+            id="question-options"
             labelKey="body"
             multiple={true}
             onChange={setOptionsToAdd}
             options={allOptions}
             placeholder="Choose an option"
           />
-          {/* <FormGroup>
-          </FormGroup> */}
         </div>
         <div
           className="button--submit"
